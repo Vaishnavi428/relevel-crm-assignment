b9d3e803fbf63cb9576f84b1b273ee3ee28ac616
VAISHNAVI GADHAVE <vgadhave411@gmail.com>
2023-05-14 23:29:09 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-// Components for each tab
-const Home = () => <h1>Home</h1>;
-const About = () => <h1>About</h1>;
-const Skills = () => <h1>Skills</h1>;
-const Projects = () => <h1>Projects</h1>;
+import { BrowserRouter as Router, Switch, Route, Link, Routes, BrowserRouter } from 'react-router-dom'// Com
+import Home from './components/Home';
+import Projects from './Projects';
+import Skills from './Skills';
+import About from './components/About';
 
 const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/skills">Skills</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+    <Routes>
+ 
+    <Route index element = {<Home />} />
+    <Route path ="about" element = {<About />} />
+    <Route path ="project" element = {<Projects />} />
+    <Route path ="skills" element = {<Skills />} />
+    
+    </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../App.css"
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    
+    <div>
+    <div className="home-container">
+    <h1>Home</h1>
+    </div>
+   <Navbar />
+  </div>
+  
+  
+  )
+}
+
+export default Home
